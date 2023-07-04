8aa265c0933aea5cbccadb812ac1d2e8e078ba54
heiwa4126 <heiwa4126@gmail.com>
2023-07-04 17:53:21 +0900

    update

--- src/components/Counter2.svelte
+++ src/components/Counter2.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	let count = 0;
+	export let count = 0;
 </script>
 
 <div class="counter">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
 	import Counter from '../components/Counter.svelte';
+	import Counter2 from '../components/Counter2.svelte';
+	import Inner from '../components/Inner.svelte';
 
 	const title = 'Welcome to Svelte';
+	let name = 'world';
+
+	let m = { x: 0, y: 0 };
+	function handleMove(event: PointerEvent) {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}
+
+	function handleMessage(event: CustomEvent<{ text: string }>) {
+		alert(event.detail.text);
+	}
+	let c1 = 1;
+	let c2 = 2;
+	let c3 = 3;
 </script>
 
 <svelte:head>
@@ -9,7 +25,26 @@
 </svelte:head>
 
 <h1>{title}</h1>
-<Counter />
-<Counter />
-<Counter />
+<div>
+	<Counter />
+</div>
+
+<div>
+	<p>{c1}+{c2}+{c3}={c1 + c2 + c3}</p>
+	<Counter2 bind:count={c1} />
+	<Counter2 bind:count={c2} />
+	<Counter2 bind:count={c3} />
+</div>
+
+<p on:pointermove={handleMove}>
+	The pointer is at {m.x} x {m.y}
+</p>
+
+<div>
+	<h2>Hello {name}!</h2>
+	<input bind:value={name} />
+</div>
+
+<p><Inner on:message={handleMessage} /></p>
+
 <p>Visit <a href="https://svelte.dev">svelte.dev</a> to read the documentation</p>
